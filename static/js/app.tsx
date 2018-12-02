0b37dcc2b0f6051afa6847df1ac030cea051dc1f
alikhachev <alikhachev@diasoft.ru>
2018-12-02 19:56:03 +0300

    add stock configurate

---
+++ static/js/app.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+// entry point for build less files
+import "../less/app";
+
+ReactDOM.render(<div className='demo'>Hello</div>, document.getElementById("example"));
